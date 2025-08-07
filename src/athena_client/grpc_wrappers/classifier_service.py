5cbd18b73e790d6ebe0d7e138c6fa9a045ba9729
snus-kin <tcarroll@snufk.in>
2025-08-07 15:19:13 +0100

    feat: feat list_deployments is now an async method

--- src/athena_client/grpc_wrappers/classifier_service.py
+++ src/athena_client/grpc_wrappers/classifier_service.py
@@ -7,14 +7,9 @@ import grpc.aio
 from google.protobuf.empty_pb2 import Empty
 
 from athena_client.generated.athena.athena_pb2 import (
-    Classification,
-    ClassificationInput,
-    ClassificationOutput,
     ClassifyRequest,
     ClassifyResponse,
-    Deployment,
     ListDeploymentsResponse,
-    RequestEncoding,
 )
 from athena_client.generated.athena.athena_pb2_grpc import ClassifierServiceStub
 
@@ -46,52 +41,8 @@ class ClassifierServiceClient:
             responses from the server.
 
         """
-        async for grpc_request in request_iter:
-            request = ClassifyRequest(
-                deployment_id=grpc_request.deployment_id,
-                inputs=[
-                    ClassificationInput(
-                        affiliate=request_input.affiliate,
-                        correlation_id=request_input.correlation_id,
-                        encoding=(
-                            RequestEncoding.Value(request_input.encoding)
-                            if isinstance(request_input.encoding, str)
-                            else request_input.encoding
-                        ),
-                        data=request_input.data,
-                    )
-                    for request_input in grpc_request.inputs
-                ],
-            )
-
-            async for grpc_response in self.stub.Classify(iter([request])):
-                yield ClassifyResponse(
-                    global_error=grpc_response.global_error,
-                    outputs=[
-                        ClassificationOutput(
-                            correlation_id=output.correlation_id,
-                            classifications=[
-                                Classification(
-                                    label=classification.label,
-                                    weight=classification.weight,
-                                )
-                                for classification in output.classifications
-                            ],
-                            error=output.error,
-                        )
-                        for output in grpc_response.outputs
-                    ],
-                )
-
-    def list_deployments(self) -> ListDeploymentsResponse:
-        """Retrieve a list of all active deployment IDs.
-
-        Returns:
-            ListDeploymentsResponse: The model representing the list
-            deployments response.
-
-        """
-        grpc_response = self.stub.ListDeployments(Empty())
+        async for grpc_response in self.stub.Classify(request_iter):
+            yield grpc_response
 
         return ListDeploymentsResponse(
             deployments=[
