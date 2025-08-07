7221fe0cad7da68c20c249a85f45f4d51f45687b
snus-kin <tcarroll@snufk.in>
2025-08-07 15:09:09 +0100

    feat: use asyncio for classify streaming endpoint

--- src/athena_client/grpc_wrappers/classifier_service.py
+++ src/athena_client/grpc_wrappers/classifier_service.py
@@ -1,6 +1,9 @@
 """Low-level GRPC client for the ClassifierService."""
 
+from collections.abc import AsyncIterable
+
 import grpc
+import grpc.aio
 from google.protobuf.empty_pb2 import Empty
 
 from athena_client.generated.athena.athena_pb2 import (
@@ -19,7 +22,7 @@ from athena_client.generated.athena.athena_pb2_grpc import ClassifierServiceStub
 class ClassifierServiceClient:
     """Low-level gRPC wrapper for the ClassifierService."""
 
-    def __init__(self, channel: grpc.Channel) -> None:
+    def __init__(self, channel: grpc.aio.Channel) -> None:
         """Initialize the client with a gRPC channel.
 
         Args:
@@ -29,52 +32,56 @@ class ClassifierServiceClient:
         """
         self.stub = ClassifierServiceStub(channel)
 
-    def classify(self, request: ClassifyRequest) -> ClassifyResponse:
+    async def classify(
+        self, request_iter: AsyncIterable[ClassifyRequest]
+    ) -> AsyncIterable[ClassifyResponse]:
         """Perform image classification in a deployment-based streaming context.
 
         Args:
-            request (ClassifyRequest): The model representing the
-            classify request.
+            request_iter (AsyncIterable[ClassifyRequest]): An async
+            iterable of classify requests to be streamed to the server.
 
-        Returns:
-            ClassifyResponse: The model representing the classify
-            response.
+        Yields:
+            AsyncIterable[ClassifyResponse]: A generator yielding classify
+            responses from the server.
 
         """
-        grpc_request = ClassifyRequest(
-            deployment_id=request.deployment_id,
-            inputs=[
-                ClassificationInput(
-                    affiliate=request_input.affiliate,
-                    correlation_id=request_input.correlation_id,
-                    encoding=(
-                        RequestEncoding.Value(request_input.encoding)
-                        if isinstance(request_input.encoding, str)
-                        else request_input.encoding
-                    ),
-                    data=request_input.data,
-                )
-                for request_input in request.inputs
-            ],
-        )
-        grpc_response = self.stub.Classify(iter([grpc_request]))
-        return ClassifyResponse(
-            global_error=grpc_response.global_error,
-            outputs=[
-                ClassificationOutput(
-                    correlation_id=output.correlation_id,
-                    classifications=[
-                        Classification(
-                            label=classification.label,
-                            weight=classification.weight,
+        async for grpc_request in request_iter:
+            request = ClassifyRequest(
+                deployment_id=grpc_request.deployment_id,
+                inputs=[
+                    ClassificationInput(
+                        affiliate=request_input.affiliate,
+                        correlation_id=request_input.correlation_id,
+                        encoding=(
+                            RequestEncoding.Value(request_input.encoding)
+                            if isinstance(request_input.encoding, str)
+                            else request_input.encoding
+                        ),
+                        data=request_input.data,
+                    )
+                    for request_input in grpc_request.inputs
+                ],
+            )
+
+            async for grpc_response in self.stub.Classify(iter([request])):
+                yield ClassifyResponse(
+                    global_error=grpc_response.global_error,
+                    outputs=[
+                        ClassificationOutput(
+                            correlation_id=output.correlation_id,
+                            classifications=[
+                                Classification(
+                                    label=classification.label,
+                                    weight=classification.weight,
+                                )
+                                for classification in output.classifications
+                            ],
+                            error=output.error,
                         )
-                        for classification in output.classifications
+                        for output in grpc_response.outputs
                     ],
-                    error=output.error,
                 )
-                for output in grpc_response.outputs
-            ],
-        )
 
     def list_deployments(self) -> ListDeploymentsResponse:
         """Retrieve a list of all active deployment IDs.
@@ -85,11 +92,12 @@ class ClassifierServiceClient:
 
         """
         grpc_response = self.stub.ListDeployments(Empty())
+
         return ListDeploymentsResponse(
             deployments=[
                 Deployment(
-                    deployment_id=grpc_response.deployment.deployment_ids,
-                    backlog=grpc_response.deployment.backlog,
+                    deployment_id=deployment.deployment_id,
+                    backlog=deployment.backlog,
                 )
                 for deployment in grpc_response.deployments
             ]
