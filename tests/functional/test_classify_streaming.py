f5afee73f9f462c0686f580843e7fdec0d64d1ba
anna-singleton-resolver <anna.singleton@crispthinking.com>
2025-09-19 17:20:10 +0100

    test: functional test for re-opening a closed stream and getting results

--- tests/functional/test_classify_streaming.py
+++ tests/functional/test_classify_streaming.py
@@ -8,6 +8,7 @@ from resolver_athena_client.client.channel import CredentialHelper
 from tests.utils.image_generation import create_random_image_generator
 from tests.utils.streaming_classify_utils import (
     classify_images,
+    classify_images_break_on_first_result,
 )
 
 
@@ -15,7 +16,7 @@ from tests.utils.streaming_classify_utils import (
 @pytest.mark.functional
 async def test_streaming_classify(
     athena_options: AthenaOptions, credential_helper: CredentialHelper
-) -> int:
+) -> None:
     """Run streaming classify functional test.
 
     Environment variables are read from .env file if present, or from the
@@ -54,12 +55,55 @@ async def test_streaming_classify(
         image_generator,
     )
 
+    assert errors == 0, f"{errors} errors occurred during stream processing"
+    assert sent == received, f"Incomplete: {sent} sent, {received} received"
+
+
+@pytest.mark.asyncio
+@pytest.mark.functional
+async def test_streaming_classify_with_reopened_stream(
+    athena_options: AthenaOptions, credential_helper: CredentialHelper
+) -> None:
+    logger = logging.getLogger(__name__)
+
+    # Configuration
+    max_test_images = int(os.getenv("TEST_IMAGE_COUNT", str(1_000)))
+    min_interval_ms = os.getenv("TEST_MIN_INTERVAL_MS", None)
+    if min_interval_ms is not None:
+        min_interval_ms = int(min_interval_ms)
+
+    image_generator = create_random_image_generator(
+        max_test_images, min_interval_ms
+    )
+
+    sent, recv, errors = await classify_images_break_on_first_result(
+        logger,
+        athena_options,
+        credential_helper,
+        image_generator,
+    )
+
+    assert sent > 0, "First stream did not send any images"
+    assert recv < sent, (
+        "First stream received all results - cannot test reopening"
+    )
+
     if errors > 0:
-        msg = f"Completed with {errors} errors"
+        msg = f"First stream returned encountered {errors} errors. Cannot "
+        "continue."
         raise AssertionError(msg)
 
-    if sent == received:
-        return 0
+    empty_generator = create_random_image_generator(0, None)
 
-    msg = f"Incomplete: {sent} sent, {received} received"
-    raise AssertionError(msg)
+    sent2, recv2, errors2 = await classify_images_break_on_first_result(
+        logger,
+        athena_options,
+        credential_helper,
+        empty_generator,
+    )
+
+    errors += errors2
+
+    assert errors == 0, f"{errors} errors occurred during stream processing"
+    assert sent2 == 0
+    assert recv2 > 0, "Second stream did not receive any results"
