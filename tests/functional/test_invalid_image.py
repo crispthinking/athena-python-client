bd1536b51f9a780628399228954f5b13186743c6
anna-singleton-resolver <anna.singleton@crispthinking.com>
2025-09-19 16:44:16 +0100

    test: add invalid image during classify stream functional test

--- tests/functional/test_invalid_image.py
+++ tests/functional/test_invalid_image.py
@@ -1,3 +1,6 @@
+import logging
+from collections.abc import AsyncIterator
+
 import pytest
 from PIL import UnidentifiedImageError
 
@@ -10,6 +13,7 @@ from resolver_athena_client.client.channel import (
 from resolver_athena_client.client.exceptions import AthenaError
 from resolver_athena_client.client.models import ImageData
 from tests.utils.image_generation import create_test_image
+from tests.utils.streaming_classify_utils import classify_images
 
 
 @pytest.mark.asyncio
@@ -93,3 +97,39 @@ async def test_classify_single_invalid_size_image(
         except Exception as e:
             msg = "Unexpected exception during invalid image test"
             raise AssertionError(msg) from e
+
+
+@pytest.mark.asyncio
+@pytest.mark.functional
+async def test_classify_streaming_one_bad_image(
+    athena_options: AthenaOptions, credential_helper: CredentialHelper
+) -> None:
+    """Functional test for ClassifyStreaming with one invalid image in the
+    stream."""
+    images = []
+    images_to_generate = 32
+    for i in range(images_to_generate):
+        if i == images_to_generate // 2:
+            images.append(create_test_image(width=500, height=500, seed=i))
+        else:
+            images.append(create_test_image(width=448, height=448, seed=i))
+
+    async def generate_images() -> AsyncIterator[ImageData]:
+        for img in images:
+            yield ImageData(img)
+
+    logger = logging.getLogger(__name__)
+
+    athena_options.resize_images = False  # set to false to trigger error
+
+    sent, recv, errors = await classify_images(
+        logger,
+        athena_options,
+        credential_helper,
+        images_to_generate,
+        generate_images(),
+    )
+
+    assert sent == images_to_generate
+    assert recv == images_to_generate - 1
+    assert errors == 1
